0abdb0d874586e7c5c2c6f7f313681e1a80d5186
discpline <dimoxernavse@gmail.com>
2024-07-20 16:58:14 +0300

    Второй коммит

--- main/views.py
+++ main/views.py
@@ -4,15 +4,18 @@ from django.http import HttpResponse
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - HOME',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_autenticated': False
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
+
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том почему этот магазин такой классный, и какой хороший товар'
+    }
+    return render(request, 'main/about.html', context)
