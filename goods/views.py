bddcf76f4da6bceb8b0834afdbeee7a5a391232e
discpline <dimoxernavse@gmail.com>
2024-07-26 16:56:25 +0300

    products url slug on site

--- goods/views.py
+++ goods/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from goods.models import Products
 
+
 def catalog(request):
 
     goods = Products.objects.all()
@@ -12,5 +13,12 @@ def catalog(request):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'goods/product.html', context=context)
